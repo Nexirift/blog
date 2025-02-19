9dfa8c3dfdfdb9e7bb4e6d9617c0d38aa3a63513
Creaous <68230846+Creaous@users.noreply.github.com>
2025-02-19 19:25:03 +1030

    Initial commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: 'https://docs.nexirift.com', title: 'Docs' },
+  { href: 'https://nexirift.com', title: 'Web' },
+]
+
+export default headerNavLinks
