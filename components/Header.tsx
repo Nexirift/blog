ad1d2e4ea1436baed70438124a3d2cae11557ff8
Creaous <68230846+Creaous@users.noreply.github.com>
2025-10-15 01:21:00 +1030

    fediverse

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,9 @@
-import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import siteMetadata from '@/data/siteMetadata'
 import Link from './Link'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -14,29 +13,29 @@ const Header = () => {
 
   return (
     <>
-      <div className="mb-4 w-full border-l-4 border-red-500 bg-red-50 p-4 shadow-sm dark:border-red-400 dark:bg-red-950/50">
+      <div className="mb-4 w-full border-l-4 border-blue-500 bg-blue-50 p-4 shadow-sm dark:border-blue-400 dark:bg-blue-950/50">
         <div className="flex items-start">
           <div className="flex-shrink-0">
             <svg
-              className="h-5 w-5 text-red-500 dark:text-red-400"
+              className="h-5 w-5 text-blue-500 dark:text-blue-400"
               viewBox="0 0 20 20"
               fill="currentColor"
               aria-hidden="true"
             >
               <path
                 fillRule="evenodd"
-                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
                 clipRule="evenodd"
               />
             </svg>
           </div>
           <div className="ml-3">
-            <p className="text-sm leading-relaxed text-red-800 dark:text-red-200">
-              <strong className="font-semibold">Notice:</strong> As of the 17th of September 2025,
-              the Nexirift project has been temporarily or permanently discontinued. Read more{' '}
+            <p className="text-sm leading-relaxed text-blue-800 dark:text-blue-200">
+              <strong className="font-semibold">Notice:</strong> We are moving to the Fediverse.
+              Posts prior to the 15th of October do not reflect the new platform. Read more{' '}
               <Link
-                href="/blog/moving-on"
-                className="font-medium underline transition-colors hover:text-red-600 dark:hover:text-red-300"
+                href="/blog/moving-on-to-the-fediverse"
+                className="font-medium underline transition-colors hover:text-blue-600 dark:hover:text-blue-300"
               >
                 here
               </Link>
