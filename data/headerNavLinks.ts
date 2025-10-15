9f9915a01b11c98404aab459b5747b1fe7355825
Creaous <68230846+Creaous@users.noreply.github.com>
2025-10-16 02:17:04 +1030

    update

--- data/headerNavLinks.ts
+++
@@ -1,7 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/staff', title: 'Staff' },
-  { href: 'https://nexirift.com', title: 'Web' },
-]
-
-export default headerNavLinks
